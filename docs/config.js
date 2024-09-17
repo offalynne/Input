f66231a388a8ff281269a51049dcaf218101c40e
alynne o. <alviscera@gmail.com>
2024-09-17 15:13:01 -0400

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -23,7 +23,7 @@ const config = {
 	name: 'Input',
 	description: 'Comprehensive cross-platform input for GameMaker 2.3',
 	latestVersion: '7.0',
-	otherVersions: ['7.0', '6.3', '6.2', '6.0', '5.6', '5.5', '5.4', '5.3', '5.2', '5.1', '5.0', '4.2', '4.1', '4.0', '3.5', '3.4'],
+	otherVersions: ['8.0', '7.0', '6.3', '6.2', '6.0', '5.6', '5.5', '5.4', '5.3', '5.2', '5.1', '5.0', '4.2', '4.1', '4.0', '3.5', '3.4'],
 	favicon: 'assets/favicon.ico',
 	themeColor: '#30dc7d',
 };
